302627dafc0e67f9025e7bfb7dd379cc18a92718
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-07-15 10:50:21 +0300

    commit tree 0b9d45371493a927d08e44bd67cd15b9b62b69d9

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,9 @@ function About() {
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src="/home/moringa/Desktop/phasetwo/labs/react-hooks-import-export-lab/images/demo.png" alt="I made this" />
     </div>
   );
 }
+
+export default About;
